a8e63db427b8bb18117ea5e408fa39aa7789e964
Synergyst <coletonm97@gmail.com>
2025-12-28 14:54:49 -0600

    Update run.sh to use tc358743_drm_present_main

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd /root/drm_overlay
 
-/root/drm_overlay/tc358743_drm_present_webui --webui-port 8080 --edid /root/drm_overlay/720P60EDID.txt --bgr
+#/root/drm_overlay/tc358743_drm_present_webui --listen 0.0.0.0 --webui-port 8080 --edid /root/drm_overlay/720P60EDID.txt --bgr
+/root/drm_overlay/tc358743_drm_present_main --listen 0.0.0.0 --webui-port 8080 --edid /root/drm_overlay/720P60EDID.txt --bgr
